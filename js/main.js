3424954f9b5ebecb62a664aad5011d7fa1e1a073
Tommy O' Peters <tommyopeters>
2019-09-02 22:17:37 +0100

    Progress

--- js/main.js
+++ js/main.js
@@ -1,10 +1,78 @@
 (function($) {
   "use strict";
 
+  class TypeWriter {
+    constructor(txtElement, words, wait = 3000) {
+      this.txtElement = txtElement;
+      this.words = words;
+      this.txt = "";
+      this.wordIndex = 0;
+      this.wait = parseInt(wait, 10);
+      this.type();
+      this.isDeleting = false;
+    }
+
+    type() {
+      // Current index of word
+
+      const current = this.wordIndex % this.words.length;
+      // Get full text of current word
+      const fullTxt = this.words[current];
+
+      // Check if deleting
+      if (this.isDeleting) {
+        // Remove char
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+        // Add char
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+
+      // Insert txt into element
+      this.txtElement.text(this.txt);
+
+      // Initial Type Speed
+      let typeSpeed = 200;
+
+      if (this.isDeleting) {
+        typeSpeed /= 2;
+      }
+
+      // If word is complete
+      if (!this.isDeleting && this.txt === fullTxt) {
+        // Make pause at end
+        typeSpeed = this.wait;
+        // Set delete to true
+        this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === "") {
+        this.isDeleting = false;
+        // Move to next word
+        this.wordIndex++;
+        // Pause before start typing
+        typeSpeed = 300;
+      }
+
+      setTimeout(() => this.type(), typeSpeed);
+    }
+  }
+
+  // Init App
+
   $(window).on("load", function() {
     $(".preloader").fadeOut("slow", function() {
       $(".preloader-left").addClass("slide-left");
     });
+
+    function init() {
+      const txtElement = $("#professions");
+      const words = JSON.parse(txtElement.attr("data-words"));
+      const wait = txtElement.attr("data-wait");
+
+      // Init TypeWriter
+      new TypeWriter(txtElement, words, wait);
+    }
+
+    init();
 
     $("#lionhero").owlCarousel({
       animateOut: "fadeOut",
